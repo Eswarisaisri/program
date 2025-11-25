f106f5919d2f3854a54ea78364892174db4a8194
Eswarisaisri <kanukollueswarisaisri@gmail.com>
2025-11-25 16:36:16 +0530

    Add files via upload

---
+++ prime8.java
@@ -0,0 +1,26 @@
+Write a program to print all factors of the Given Number.
+
+import java.util.Scanner;
+class prime8
+{
+    public static void main(String []args)
+    {
+        //Write your code here
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n<=0)
+        {
+            System.out.println("Invalid Input");
+        }
+        else
+        {
+            for(int i=1;i<=n;i++)
+            {
+                if(n%i==0)
+                {
+                    System.out.print(i+" ");
+                }
+            }
+        }
+    }
+}
